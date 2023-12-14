c7094d5873d3491c0d58fc76681acd00acbd2dcb
Nikolai O <nik@o84.ru>
2023-12-14 23:44:09 +0300

    feat: add carbon dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,23 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let t = 0;
+  let sample = 0;
+  const k = 0.693 / HALF_LIFE_PERIOD;
+  // if (typeof sampleActivity === 'string' && sampleActivity !== '' && sampleActivity !== null && sampleActivity !== undefined && sampleActivity !== NaN) {
+  if (typeof sampleActivity === 'string' && sampleActivity !== '') {
+      sample = Number(sampleActivity);
+    if (sample > 0 && sample < 15) {
+      t = Math.log(MODERN_ACTIVITY / sample);
+      t = t / k;
+      return Math.ceil(t);
+    }
+    return false;
+  }
+  return false;
 }
 
 module.exports = {
