f4d4f83973c7806fb2c5120a55d7b603067c4b5c
Nikolai O <nik@o84.ru>
2023-12-17 22:32:10 +0300

    feat: add transform array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,11 +13,50 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
+function transform(arr) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  if (Array.isArray(arr) === true ) {
+    newArr = [];
+    let prevItem = 0, nextItem = 0;
+    for (let i = 0; i <= arr.length; i += 1){
+      prevItem = arr[i - 1];
+      nextItem = arr[i + 1];
+      // console.log(arr, typeof prevItem, 'prevItem=', prevItem, typeof nextItem, 'nextItem=', nextItem);
+      if (typeof nextItem === 'string' || typeof prevItem === 'string') {
+        //prev
+        //if (nextItem !== '--discard-prev' && prevItem !== '--discard-next' && nextItem !== '--discard-next') {
+        if (nextItem !== '--discard-prev' && prevItem !== '--discard-next') {
+          if (arr[i] !== undefined && (typeof arr[i] !== 'string' || (arr[i] !== '--discard-prev' && arr[i] !== '--discard-next' && arr[i] !== '--double-prev' && arr[i] !== '--double-next'))) {
+            //console.log('string', 'prevItem=', prevItem, 'nextItem=', nextItem);
+            newArr.push(arr[i]);
+          }
+        }
+        if (nextItem === '--double-prev' && prevItem !== '--discard-next') {
+          //console.log('string double-prev', 'prevItem=', prevItem, 'nextItem=', nextItem);
+          if (arr[i] !== undefined) {
+            newArr.push(arr[i]);
+          }
+        }
+        if (prevItem === '--double-next') {
+          //console.log('string double-next', 'prevItem=', prevItem, 'nextItem=', nextItem);
+          if (arr[i] !== undefined) {
+            newArr.push(arr[i]);
+          }
+        }
+      } else {
+        // current
+        if (arr[i] !== undefined && (typeof arr[i] !== 'string' || (arr[i] !== '--discard-prev' && arr[i] !== '--discard-next' && arr[i] !== '--double-prev' && arr[i] !== '--double-next'))) {
+          //console.log('current');
+          newArr.push(arr[i]);
+        }
+      }
+    }
+    return newArr;
+  } else {
+    throw new NotImplementedError(`'arr' parameter must be an instance of the Array!`);
+  }
 }
-
 module.exports = {
   transform
 };
