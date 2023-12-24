c8a737a93f994350d2f2750ffc201fcfb172a8e5
Nikolai O <nik@o84.ru>
2023-12-24 13:29:04 +0300

    feat: add common character count task

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,9 +11,43 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  let count = 0, indexBig = 0, indexSmall = 0, string = '', length = 0;
+  let arrayBig = [];
+  let arraySmall = [];
+  if (typeof s1 !== undefined && typeof s2 !== undefined) {
+    if (s1.leigth > s2.leigth) {
+      arrayBig = Array.from(s1);
+      arraySmall = Array.from(s2);
+      string = s2;
+    } else {
+      arrayBig = Array.from(s2);
+      arraySmall = Array.from(s1);
+      string = s1;
+    }
+    length = arrayBig.length;
+    //
+    //console.log('arraySmall=', arraySmall, 'arrayBig=', arrayBig, 'arrayBig length==', length);
+    for (let i = 0; i < length; i += 1) {
+      //console.log(i, 'string', string, 'include arrayBig[i]', arrayBig[i]);
+      if (string.includes(arrayBig[i])) {
+        count += 1;
+        //console.log(i, 'found arrayBig[i]', arrayBig[i], 'count=', count);
+        indexBig = arrayBig.indexOf(arrayBig[i]);
+        indexSmall = arraySmall.indexOf(arrayBig[i]);
+        // del from arrayBig
+        arrayBig.splice(indexBig, 1);
+        //console.log('shift arrayBig, now:', arrayBig);
+        arraySmall = Array.from(string);
+        // del from arraySmall
+        arraySmall.splice(indexSmall, 1);
+        string = arraySmall.join('');
+        //console.log('shift arraySmall, now:', arraySmall);
+        i = i - 1;
+      }
+    }
+    return count;
+  }
 }
 
 module.exports = {
