f66ac959c41164985fc301d539e10fb095a4b50e
Nikolai O <nik@o84.ru>
2023-12-24 15:00:55 +0300

    feat: add delete digits task

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,10 +11,39 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  const arrayD = Array.from(String(n));
+  let arrayDel = [];
+  let string = '', arrayIndex = '';
+  let result = 0;
+  Object.freeze(arrayD);
+  //console.log(Object.isFrozen(array));
+  for (let i = 0; i < arrayD.length; i += 1) {
+    //console.log('ArrayConst=', arrayD);
+    arrayD.forEach((item) => {
+      arrayDel.push(item);
+    });
+    arrayIndex = arrayDel.indexOf(arrayDel[i]);
+    arrayDel.splice(arrayIndex, 1);
+    //string = Number(arrayDel.join(''));
+    string = arrayDel.join('');
+    if (string > result){
+      result = string;
+    }
+    //console.log('arrayD=', arrayD, 'arrayDel=', arrayDel, 'string=', string, 'result=', result);
+    arrayDel = [];
+  }
+  return Number(result);
 }
+
+console.log(deleteDigit(152));//, 52);
+//console.log(deleteDigit(1001));//, 101);
+//console.log(deleteDigit(10));//, 1);
+//console.log(deleteDigit(222219));//, 22229);
+//console.log(deleteDigit(109));//, 19);
+//console.log(deleteDigit(342));//, 42);
+
+// console.log();
 
 module.exports = {
   deleteDigit
